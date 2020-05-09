a411d4988c59b692d24377f2c85d8fdd14b88ac9
Devin Smith <devin@devinsmith.net>
2020-05-08 17:40:49 -0700

    A little more work on xlib, doesn't work still.

--- source/vga_xlib.c
+++ source/vga_xlib.c
@@ -49,17 +49,65 @@ static const SDL_Color sdl_palette[] = {
 };
 #endif
 
-Display *dpy;
-int screen;
-Window root;
-XVisualInfo *vi;
+static Display *dpy;
+static int screen, index_mode;
+static Window root, win;
+static XVisualInfo *vi;
+static Colormap cmap;
+static XColor clr[256];
+static GC gc;
+static uint8_t *framebuffer;
+static uint8_t *imagebuffer;
+static XImage *img;
+
+static int detect_visual(int depth, int class)
+{
+  XVisualInfo vinfo;
+  int i, num_visuals;
+
+  vinfo.screen = screen;
+  vinfo.depth = depth;
+  vinfo.class = class;
+
+  vi = XGetVisualInfo(dpy, VisualScreenMask | VisualDepthMask |
+      VisualClassMask, &vinfo, &num_visuals);
+  if (vi && (num_visuals > 0)) {
+    index_mode = depth == 8 ? 1 : 0;
+
+    printf("%d: rm:%lu gm:%lu bm:%lu cs:%d bpr:%d\n", depth, vi->red_mask,
+        vi->green_mask, vi->blue_mask, vi->colormap_size, vi->bits_per_rgb);
+
+    cmap = XCreateColormap(dpy, root, vi->visual, AllocAll);
+    if (index_mode == 1) {
+      for (i = 0; i < 256; i++) {
+        clr[i].pixel = i;
+        clr[i].flags = DoRed | DoGreen | DoBlue;
+      }
+    }
+
+    return 1;
+  }
+  return 0;
+}
+
+static int setup_visual_colormap()
+{
+  return detect_visual(8, PseudoColor) ||
+    detect_visual(15, TrueColor) ||
+    detect_visual(16, TrueColor) ||
+    detect_visual(24, TrueColor) ||
+    detect_visual(32, TrueColor);
+}
 
 int
 display_start(int game_width, int game_height)
 {
   const char *disp_env;
-  XVisualInfo vinfo;
-  int num_visuals;
+  XSetWindowAttributes attr;
+  XGCValues gcvalues;
+  XSizeHints sizehints;
+  int attrmask;
+  int bpp;
 
   disp_env = getenv("DISPLAY");
   dpy = XOpenDisplay(disp_env);
@@ -70,25 +118,75 @@ display_start(int game_width, int game_height)
   screen = DefaultScreen(dpy);
   root = RootWindow(dpy, screen);
 
-  vinfo.screen = screen;
-  vinfo.depth = 8;
-  vinfo.class = PseudoColor;
-
-  vi = XGetVisualInfo(dpy, VisualScreenMask | VisualDepthMask |
-      VisualClassMask, &vinfo, &num_visuals);
-
-  if ((vi == NULL) || (num_visuals == 0)) {
-    fprintf(stderr, "No visuals found!\n");
+  if (setup_visual_colormap() == 0) {
+    fprintf(stderr, "No usable visual found!\n");
     return -1;
   }
 
-  if (vi->class != PseudoColor) {
-    fprintf(stderr, "Currently no support for non-TrueColor visuals.\n");
+  attr.colormap = cmap;
+  attr.event_mask = KeyPressMask | KeyReleaseMask | ExposureMask;
+  attrmask = CWColormap | CWEventMask;
+  win = XCreateWindow(dpy, root, 0, 0, 320, 200, 0, CopyFromParent,
+      InputOutput, vi->visual, attrmask, &attr);
+  if (win == None) {
+    fprintf(stderr, "Could not create window!\n");
     return -1;
   }
 
+  gcvalues.foreground = BlackPixel(dpy, screen);
+  gcvalues.background = WhitePixel(dpy, screen);
+  gc = XCreateGC(dpy, win, GCForeground | GCBackground, &gcvalues);
 
+  sizehints.min_width = 320;
+  sizehints.min_height = 200;
+  sizehints.max_width = 320;
+  sizehints.max_height = 200;
+  sizehints.base_width = 320;
+  sizehints.base_height = 200;
+  sizehints.flags = PMinSize | PMaxSize | PBaseSize;
 
+  //XSetWMProperties(dpy, win, NULL, NULL, NULL, 0, &sizehints, None, None);
+  XStoreName(dpy, win, "OpenDW");
+  XSetIconName(dpy, win, "OpenDW");
+
+  switch (vi->depth) {
+  case 15:
+  case 16:
+    bpp = 2;
+    break;
+  case 24:
+    bpp = 3;
+    break;
+  case 32:
+    bpp = 4;
+    break;
+  default:
+    bpp = 1;
+    break;
+  }
+
+  if ((framebuffer = calloc(VGA_WIDTH * VGA_HEIGHT, 1)) == NULL) {
+    fprintf(stderr, "Framebuffer could not be allocated.\n");
+    return -1;
+  }
+
+  if (index_mode) {
+    imagebuffer = framebuffer;
+  } else {
+    if ((imagebuffer = calloc(VGA_WIDTH * VGA_HEIGHT, bpp)) == NULL) {
+      fprintf(stderr, "Image buffer could not be allocated.\n");
+      return -1;
+    }
+  }
+
+  img = XCreateImage(dpy, vi->visual, vi->depth, ZPixmap, 0,
+      (char *)imagebuffer, 320, 200, 8, 320 * bpp);
+  if (img == NULL) {
+    fprintf(stderr, "Could not create X11 pixmap image.\n");
+    return -1;
+  }
+
+  XMapWindow(dpy, win);
 
   return 0;
 }
@@ -110,7 +208,7 @@ void waitkey()
 static uint8_t *
 get_fb_mem()
 {
-  return NULL;
+  return framebuffer;
 }
 
 struct vga_driver x_driver = {
