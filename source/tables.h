3ba8b50217780a4d14bd4372abc4ca5a8496f37d
Devin Smith <devin@devinsmith.net>
2020-07-24 16:49:03 -0700

    Add additional table for sub_DEB (upcoming).

--- source/tables.h
+++ source/tables.h
@@ -31,6 +31,7 @@ uint8_t get_or_table(uint8_t offset);
 uint8_t get_1BC1_table(uint8_t offset);
 uint16_t get_line_offset(uint8_t offset);
 uint16_t get_unknown_4456(uint8_t index);
+uint16_t get_and_table_B452(uint8_t offset);
 
 #ifdef __cplusplus
 }
