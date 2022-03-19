d17f55ebef74ab5a27c541deb51cebc6fdad7e18
Devin Smith <devin@devinsmith.net>
2022-03-19 10:09:38 -0700

    Change le16bit util into general purpose extractor

--- src/tools/le16bit.cpp
+++ src/tools/le16bit.cpp
@@ -1,22 +1,43 @@
+// Tool for extracting contents out of dragon.com
+// and creating constant tables in C / C++ code.
+//
+// Yes, xxd can probably handle this with the right
+// arguments...
+
 #include <cstdio>
 #include <cstdint>
 
-// Little endian 16 bit extractor
-//
-// I couldn't figure out how to do this with xxd :(
-int main(void)
+void extract_b152(FILE *fp)
 {
-  FILE *fp = fopen("DRAGON.COM", "rb");
-  uint16_t com_start = 0xB652;
+  uint16_t com_start = 0xB152;
 
-  if (fp == NULL) {
-    printf("Cannot open DRAGON.COM\n");
-    return -1;
+  if (fseek(fp, com_start - 0x100, SEEK_SET) != 0) {
+    printf("Can't seek to offset\n");
+    return;
   }
+
+  printf("static uint8_t b152[256] = {\n ");
+  for (int i = 0; i < (256 / 8); i++)
+  {
+    for (int j = 0; j < 8; j++) {
+      uint8_t dval = fgetc(fp);
+      printf(" ");
+      printf("0x%02X,", dval);
+    }
+    printf(" // 0x%04X-0x%04X\n ", com_start, com_start + 7);
+    com_start += 8;
+  }
+  printf("\r};\n");
+}
+
+// Little endian 16 bit extractor
+void extract_b652(FILE *fp)
+{
+  uint16_t com_start = 0xB652;
 
   if (fseek(fp, 0xB552, SEEK_SET) != 0) {
     printf("Can't seek to offset\n");
-    goto done;
+    return;
   }
 
   printf("static uint16_t or_table_B652[256] = {\n ");
@@ -32,8 +53,22 @@ int main(void)
     com_start += 0x10;
   }
   printf("\r};\n");
+}
 
-done:
+
+
+int main(void)
+{
+  FILE *fp = fopen("DRAGON.COM", "rb");
+
+  if (fp == NULL) {
+    printf("Cannot open DRAGON.COM\n");
+    return -1;
+  }
+
+  extract_b152(fp);
+  //extract_b652(fp);
+
   fclose(fp);
 
   return 0;
